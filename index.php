06fb59e3396181df957f71e822bdd40428812896
DJDERNANE <108133683+DJDERNANE@users.noreply.github.com>
2022-11-22 18:14:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,198 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LYDIA Hotel</title>
+    <link rel="stylesheet" href="style/style.css">
 </head>
+
 <body>
-    hello php
+    <div class="main-parent">
+        <div class="main">
+            <div class="header">
+                <div class="logo">
+                    <img src="imgs/logo.png" alt="">
+                </div>
+                <div class="navbar">
+                    <ul>
+                        <li>
+                            <a href="">
+                                HOME
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                ROOMS
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                GALLERY
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                CONTACT US
+                            </a>
+                        </li>
+                       
+                    </ul>
+                    <div class="btns">
+                        <button>BOOK NOW <img src="imgs/Vector.png" alt=""></button>
+                        <button>HOTEL WITH FLIGHT </button>
+                    </div>
+
+                </div>
+            </div>
+            <div class="fond">
+                <p>Indoors & Outdoors</p>
+                <h1>Enjoy a Luxury Lxperience</h1>
+                <button>ROOMS & SUITS </button>
+            </div>
+
+        </div>
+    </div>
+    
+
+    <div class="lydiacity">
+        <form action="">
+            <h6>ONE OF THE BEST PLACES TO RELAX & ENJOY</h6> 
+            <h1>Lydia City Hotel Rhodes</h1> 
+            <div class="formulaire">
+                <div>
+                    <label for="">Your Name:</label>
+                    <input type="text" placeholder="Enter Your Name">
+                </div>
+                <div>
+                    <label for="">Email:</label>
+                    <input type="text" placeholder="Your Email ID">
+                </div>
+                <div>
+                    <label for="">Check In:</label>
+                    <input type="date" placeholder="Date">
+                </div>
+                <div>
+                    <label for="">Check Out:</label>
+                    <input type="date" placeholder="Date">
+                </div>
+                <div>
+                    <label for="">Rooms:</label>
+                    <select name="" id="" placeholder="select">
+                        <option value="" selected>Select</option>
+                        <option value="">2</option>
+                        <option value="">3</option>
+                        <option value="">4</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="">Adults:</label>
+                    <input type="text" placeholder="Adult Persones ">
+                </div>
+            </div>
+            <div class="agree">
+                <input type="checkbox" name="" id="read">
+                <label for="read">I have read and agree with the terms</label>
+            </div>
+            <div class="btn">
+                <button>SEND</button> 
+            </div>   
+        </form>
+    </div>
+
+
+    <div class="about">
+        <div class="txt">
+            <h6>ABOUT LYDIA HOTEL</h6>
+            <h1>Enoy a luxury experience</h1>
+            <p>Lorem ipsum dolor sit amet consectetur. Pretium gravida non integer magna. Nibh facilisi eget justo tristique. Nunc purus vehicula curabitur egestas. Vel eget est semper ut. Nibh duis nec in at nulla sit. Congue ut commodo eu nunc. Tincidunt proin nec lobortis imperdiet auctor id aliquet. Morbi dignissim scelerisque est erat. <br>
+                Tincidunt eu porta vivamus id magnis tellus volutpat aliquet. Velit ut non vitae ornare. Pretium interdum volutpat bibendum malesuada odio nulla tellus. Tortor cursus quam elit sagittis in. Arcu ut magna arcu sit. Leo dignissim neque luctus ornare volutpat neque. Morbi integer aliquam fermentum diam ac. Nunc.</p>
+        </div>
+        <div class="pic">
+            <img src="imgs/img1.svg" alt="">
+            <img src="imgs/img2.svg" alt="">
+        </div> 
+    </div>
+
+
+    <div class="service-parent">
+        <h6>WHAT WE PROVIDE</h6>
+        <h1>Our Services</h1>
+        <div class="services">
+            <div class="child">
+                <img src="imgs/sleep.svg" alt="">
+                <h6>Room Service</h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur. Pretium gravida non integer magna. Nibh facilisi eget justo tristique. Nunc purus vehicula curabitur egestas. Vel eget est semper ut. Nibh duis nec in at nulla sit. Congue ut commodo eu nunc. Tincidunt proin nec lobortis 
+                </p>
+            </div>
+            <div class="child">
+                <img src="imgs/bain.svg" alt="">
+                <h6>Luxury Bathroom</h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur. Pretium gravida non integer magna. Nibh facilisi eget justo tristique. Nunc purus vehicula curabitur egestas. Vel eget est semper ut. Nibh duis nec in at nulla sit. Congue ut commodo eu nunc. Tincidunt proin nec lobortis 
+                </p>
+            </div>
+            <div class="child">
+                <img src="imgs/tv.svg" alt="">
+                <h6>Setellite Television Set </h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur. Pretium gravida non integer magna. Nibh facilisi eget justo tristique. Nunc purus vehicula curabitur egestas. Vel eget est semper ut. Nibh duis nec in at nulla sit. Congue ut commodo eu nunc. Tincidunt proin nec lobortis 
+                </p>
+            </div>
+            <div class="child">
+                <img src="imgs/wifi.svg" alt="">
+                <h6>Wifi</h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur. Pretium gravida non integer magna. Nibh facilisi eget justo tristique. Nunc purus vehicula curabitur egestas. Vel eget est semper ut. Nibh duis nec in at nulla sit. Congue ut commodo eu nunc. Tincidunt proin nec lobortis 
+                </p>
+            </div>
+            <div class="child">
+                <img src="imgs/cofee.svg" alt="">
+                <h6>Cofee</h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur. Pretium gravida non integer magna. Nibh facilisi eget justo tristique. Nunc purus vehicula curabitur egestas. Vel eget est semper ut. Nibh duis nec in at nulla sit. Congue ut commodo eu nunc. Tincidunt proin nec lobortis 
+                </p>
+            </div>
+            <div class="child">
+                <img src="imgs/sleep.svg" alt="">
+                <h6>Baekfast</h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur. Pretium gravida non integer magna. Nibh facilisi eget justo tristique. Nunc purus vehicula curabitur egestas. Vel eget est semper ut. Nibh duis nec in at nulla sit. Congue ut commodo eu nunc. Tincidunt proin nec lobortis 
+                </p>
+            </div>
+            
+        </div>
+        
+    </div>
+
+
+    <div class="achivement-parent">
+        <h6>AWARDS</h6>
+        <h1>Our Achievements</h1>
+        <div class="achivement">
+            <img src="imgs/img3.svg" alt="">
+            <img src="imgs/img5.svg" alt="">
+            <img src="imgs/img6.svg" alt="">
+            <img src="imgs/img6.svg" alt="">
+            <img src="imgs/img6.svg" alt="">
+        </div>
+        <button>VISITE THE SITE</button>
+    </div>
+
+    <div class="Specialite-parent">
+        <div class="specialite">
+            <h6>WHY CHOOSE US?</h6>
+            <h1>Our Specialities</h1>
+            <p>Situated at the very heart of Rhodes town, the hotel provides easy access to all major points of interest all within walking distance.
+                The hotel is renowned in the local market due to its operation which “runs” 365 days a year. All rooms have been renovated recently and offer a unique design with modern furnishes. Being in constant operation for the last 47 years, and gone through numerous renovations the hotel operates mainly with repeaters who value and appreciate its comfort, cleanliness, family attitude, amenities and personality just to name a few.
+                On line booking is available through mainstream agencies or directly with the hotel, with its own platform at www.lydiahotel.com were various offers are available all through the year.
+                Our reception operates 24 hours and 7 days a week, making check in or providing information a breeze. All the information one might need for sightseeing the island either by boat, bike, car or motorbike is available there. Tickets for shows, boat trips or museums are also available, as well as car hire from the hotels own rental company, offering lower rates than any competitor local or otherwise.</p>
+        </div>
+
+    </div>
 </body>
+
 </html>
